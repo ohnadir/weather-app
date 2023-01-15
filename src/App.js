c57c65ebecb46ab4bec650b3ca007317c54f7083
Nadir Hossain <nadirhossain336@gmail.com>
2023-01-15 13:53:11 +0600

    uploaded assets

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ function App() {
   return (
     <div className="">
       
+      <h2>Nadir </h2>
+      
     </div>
   );
 }
---
+++ src/components/weather.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import hotBg from "./Assets/hotBg.jpg";
+import coldBg from "./Assets/cold.jpg";
+
+const weather = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default weather
