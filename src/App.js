2aca63e3fbd3ce4c7e80afdf215bf92fb2adb68b
Nadir Hossain <nadirhossain336@gmail.com>
2023-01-16 12:12:42 +0600

    implement navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
-  
   return (
-    <div className="">
-      
-      <h2>Nadir </h2>
-      
+    <div className="mx-auto max-w-screen-md py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700  h-fit shadow-xl shadow-gray-400">
+      <Navbar/>
     </div>
   );
 }
