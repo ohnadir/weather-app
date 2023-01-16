8c33e422c0ed8abb76de2f2f7f3a8934ef035234
Nadir Hossain <nadirhossain336@gmail.com>
2023-01-16 22:12:25 +0600

    implement forcast

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Inputs from './components/Inputs';
 import Navbar from './components/Navbar';
 import TemperatureAndDetails from './components/TemperatureAndDetails';
 
+
 function App() {
   return (
     <div className="mx-auto max-w-screen-md py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700  h-fit shadow-xl shadow-gray-400">
