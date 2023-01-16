f3a60217979bde798f1599d4941f4f978c514eb6
Nadir Hossain <nadirhossain336@gmail.com>
2023-01-16 12:30:06 +0600

    implement inputs component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Inputs from './components/Inputs';
 import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="mx-auto max-w-screen-md py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700  h-fit shadow-xl shadow-gray-400">
       <Navbar/>
+      <Inputs/>
     </div>
   );
 }
