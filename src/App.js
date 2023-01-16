0bbafd5ed1027cfe452aa76e859bc695ca020e5f
Nadir Hossain <nadirhossain336@gmail.com>
2023-01-16 12:36:05 +0600

    implement date and location component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import DateAndLocation from './components/DateAndLocation';
 import Inputs from './components/Inputs';
 import Navbar from './components/Navbar';
 
@@ -7,6 +8,7 @@ function App() {
     <div className="mx-auto max-w-screen-md py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700  h-fit shadow-xl shadow-gray-400">
       <Navbar/>
       <Inputs/>
+      <DateAndLocation/>
     </div>
   );
 }
