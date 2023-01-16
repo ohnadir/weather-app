8ea727d8b454d61bafec37a68efb43def11ce0ea
Nadir Hossain <nadirhossain336@gmail.com>
2023-01-16 14:22:32 +0600

    implement temperature and details

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import DateAndLocation from './components/DateAndLocation';
 import Inputs from './components/Inputs';
 import Navbar from './components/Navbar';
+import TemperatureAndDetails from './components/TemperatureAndDetails';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Navbar/>
       <Inputs/>
       <DateAndLocation/>
+      <TemperatureAndDetails/>
     </div>
   );
 }
---
+++ src/components/TemperatureAndDetails.jsx
@@ -0,0 +1,74 @@
+import React from 'react';
+import {
+    UilTemperature,
+    UilTear,
+    UilWind,
+    UilSun,
+    UilSunset,
+  } from "@iconscout/react-unicons";
+
+const TemperatureAndDetails = () => {
+    return (
+        <div>
+      <div className="flex items-center justify-center py-6 text-xl text-cyan-300">
+        <p>details</p>
+      </div>
+
+      <div className="flex flex-row items-center justify-between text-white py-3">
+        <img src="" alt="" className="w-20" />
+        <p className="text-5xl">36° </p>
+        <div className="flex flex-col space-y-2">
+          <div className="flex font-light text-sm items-center justify-center">
+            <UilTemperature size={18} className="mr-1" />
+            Real fell:
+            <span className="font-medium ml-1">24° </span>
+          </div>
+          <div className="flex font-light text-sm items-center justify-center">
+            <UilTear size={18} className="mr-1" />
+            Humidity:
+            <span className="font-medium ml-1">40%</span>
+          </div>
+          <div className="flex font-light text-sm items-center justify-center">
+            <UilWind size={18} className="mr-1" />
+            Wind:
+            <span className="font-medium ml-1"> km/h</span>
+          </div>
+        </div>
+      </div>
+
+      <div className="flex flex-row items-center justify-center space-x-2 text-white text-sm py-3">
+        <UilSun />
+        <p className="font-light">
+          Rise:{" "}
+          <span className="font-medium ml-1">
+          mpppp
+          </span>
+        </p>
+        <p className="font-light">|</p>
+
+        <UilSunset />
+        <p className="font-light">
+          Set:{" "}
+          <span className="font-medium ml-1">mpppp
+          </span>
+        </p>
+        <p className="font-light">|</p>
+
+        <UilSun />
+        <p className="font-light">
+          High:{" "}
+          <span className="font-medium ml-1">36° </span>
+        </p>
+        <p className="font-light">|</p>
+
+        <UilSun />
+        <p className="font-light">
+          Low:{" "}
+          <span className="font-medium ml-1">36° </span>
+        </p>
+      </div>
+    </div>
+    )
+}
+
+export default TemperatureAndDetails
