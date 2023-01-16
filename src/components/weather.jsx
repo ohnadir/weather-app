7168cbeae06864501d5b829cc032a554cdbc30c4
Nadir Hossain <nadirhossain336@gmail.com>
2023-01-16 11:47:25 +0600

    import google fonts

--- src/components/weather.jsx
+++ src/components/weather.jsx
@@ -2,7 +2,16 @@ import React, { useState } from 'react';
 import hotBg from "./Assets/hotBg.jpg";
 import coldBg from "./Assets/cold.jpg";
 
-const weather = () => {
+const weather = async() => {
+    const API_KEY ="b4ce7e2e75269055856ae3f636fa8bbb";
+
+const city = "Dhaka";
+const units= "metric"
+const URL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=${units}`;
+const data = await fetch(URL)
+    .then((res) => res.json())
+    .then((data) => console.log(data));
+    console.log(data)
     return (
         <div>
 
