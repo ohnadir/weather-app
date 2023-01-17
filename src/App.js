2883eb3ab0f10e62e4a2951b1496c8530a335bd5
Nadir Hossain <nadirhossain336@gmail.com>
2023-01-17 14:31:37 +0600

    implement weather api

--- src/App.js
+++ src/App.js
@@ -3,9 +3,15 @@ import DateAndLocation from './components/DateAndLocation';
 import Inputs from './components/Inputs';
 import Navbar from './components/Navbar';
 import TemperatureAndDetails from './components/TemperatureAndDetails';
+import getFormattedWeatherData from './service';
 
 
 function App() {
+  const fetchWeaterData = async()=>{
+    const data = await getFormattedWeatherData({ q: "london"})
+    console.log(data)
+  }
+  fetchWeaterData();
   return (
     <div className="mx-auto max-w-screen-md py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700  h-fit shadow-xl shadow-gray-400">
       <Navbar/>
--- src/components/Inputs.jsx
+++ src/components/Inputs.jsx
@@ -10,7 +10,7 @@ const Inputs = () => {
             <input
             type="text"
             placeholder="Search for city...."
-            className="text-xl font-light p-2 w-full shadow-xl focus:outline-none capitalize placeholder:lowercase"
+            className="text-lg font-light p-2 w-full shadow-xl focus:outline-none capitalize placeholder:lowercase"
             />
             <BiSearch 
                 size={25}
